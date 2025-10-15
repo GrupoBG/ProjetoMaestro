a5580c3e4138e2fb40ad75f138b25c11c4b4ea7e
God-rep <208394789+God-rep@users.noreply.github.com>
2025-10-15 12:12:06 -0300

    Add files via upload

--- testes/teste3/main.c
+++ testes/teste3/main.c
@@ -16,7 +16,7 @@
 typedef struct {
     int x, y;
     int radius;
-    int partition;      // Diz a qual particao o circulo pertence. partition<0 -> invalido
+    int partition;	// Diz a qual particao o circulo pertence. partition<0 -> invalido
 
     int starting_tick; // Diz quantos ticks de espera em relacao ao ultimo circulo
 
@@ -73,29 +73,29 @@ int main(int argc, char* argv[]) {
 
     Uint32 next_note_tick;
 
-        /*      Declaracoes SDL         */
+	/*	Declaracoes SDL		*/
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
 
 
-        /*      Iniciacao de globais    */
+	/*      Iniciacao de globais    */
     srand(time(NULL));  // Gera chave aleatoria
 
     // Inicia globais
 
     initiate_circle_array(circle_array, 100);
-    fill_circle_array(partiture, partiture_end);
+    fill_circle_array(partiture, partiture_end+1);
 
     next_note_tick = partiture[0].starting_tick;
 
 
-        /*      Iniciacao do SDL        */
+	/*	Iniciacao do SDL	*/
 
 
     sdl_init_code = SDL_Init(SDL_INIT_VIDEO);
     if (sdl_init_code < 0) {
         printf("Erro ao inicializar SDL: %s\n", SDL_GetError());
-        output = -1;
+	output = -1;
         goto FIM;
     }
 
@@ -116,95 +116,96 @@ int main(int argc, char* argv[]) {
         goto FIM;
     }
 
-        /*      Execucao        */
+    	/*	Execucao	*/
 
     while (state) {
 
-        // Checa se acabou a musica
-        if(partiture_next > partiture_end){
+	// Checa se acabou a musica
+	if(partiture_next > partiture_end){
                 state = 0;
 
-                break;
+        	break;
         }
 
 
-        // Tenta gerar novos circulos
-        if (next_note_tick <= tick_counter
-        && ((circle_array_end + 1)%100) != circle_array_begin){
+	// Tenta gerar novos circulos
+	if ( (next_note_tick <= tick_counter)
+	&& (((circle_array_end + 1)%100) != circle_array_begin) ){
 
                 // Cria circulo
                 circle_array[circle_array_end] = partiture[partiture_next];
                 ++circle_array_end;
                 circle_array_end %= 100;
 
-                // Atualiza proximo circulo na partitura
+		// Atualiza proximo circulo na partitura
                 ++partiture_next;
 
-                // Atualiza proximo circulo na partitura e tempo da ultima nota
+		// Atualiza proximo circulo na partitura e tempo da ultima nota
                 next_note_tick = tick_counter + partiture[partiture_next].starting_tick;
 
 
                 printf("\033[0;32mCirculo criado! (espacos possiveis para novos circulo: %d )\033[0m\n",
-        ((circle_array_begin-circle_array_end)>=0)?(100-(100-circle_array_begin+circle_array_end)):(100-circle_array_end-circle_array_begin));
-        }
+	((circle_array_end-circle_array_begin)>=0)?(100-(circle_array_end-circle_array_begin)):100-(100-circle_array_begin+circle_array_end));
+	}
 
 
-        // Tenta limpar circulos expirados do buffer
-        while (  (circle_array[circle_array_begin].remaining_ticks < 0)
+	// Tenta limpar circulos expirados do buffer
+	while (  (circle_array[circle_array_begin].remaining_ticks < 0)
         && (circle_array_begin != circle_array_end)){
                 ++circle_array_begin;
                 circle_array_begin %= 100;
 
                 // Avisa na tela
                 printf("\033[0;33mCirculo consumido do buffer! (circulos restantes: %d)\033[0m\n",
-        ((circle_array_end-circle_array_begin)>=0)?(circle_array_end-circle_array_begin):100-(circle_array_end-circle_array_begin) );
+        ((circle_array_end-circle_array_begin)>=0)?(circle_array_end-circle_array_begin):(100-circle_array_begin+circle_array_begin) );
 
         }
 
 
-                /*      Desenho         */
-        // Fundo
+
+		/*      Desenho         */
+	// Fundo
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Circulos
-        for(int i = circle_array_begin; i != circle_array_end; i=((i+1)%100)){
+	// Circulos
+	for(int i = circle_array_begin; i != circle_array_end; i=((i+1)%100)){
 
-                //printf("Desenhando circulos!\n");
+		//printf("Desenhando circulos!\n");
 
-                // Atualiza circulos com ticks
-                if(circle_array[i].remaining_ticks >=0){
+		// Atualiza circulos com ticks
+		if(circle_array[i].remaining_ticks >=0){
 
-                        // Efeito de fade in
-                        if((circle_array[i].remaining_ticks) > (circle_array[i].base_ticks/2)){
+			// Efeito de fade in
+			if((circle_array[i].remaining_ticks) > (circle_array[i].base_ticks/2)){
 
-                                if( 255 < (circle_array[i].color.a + ( 255/(circle_array[i].base_ticks/2) ) ) ){
+				if( 255 < (circle_array[i].color.a + ( 255/(circle_array[i].base_ticks/2) ) ) ){
                                         circle_array[i].color.a = 255;
                                 }
-                                else{
-                                        circle_array[i].color.a += ( 255/(circle_array[i].base_ticks/2) );
-                                }
-                        }
+				else{
+					circle_array[i].color.a += ( 255/(circle_array[i].base_ticks/2) );
+				}
+			}
 
-                        // Efeito de fade out
-                        else{
-                                if(circle_array[i].color.a > ( 255/(circle_array[i].base_ticks/2))){
-                                        circle_array[i].color.a -= ( 255/(circle_array[i].base_ticks/2) );
-                                }
-                                else{
-                                        circle_array[i].color.a = 0;
-                                }
-                        }
+			// Efeito de fade out
+			else{
+				if(circle_array[i].color.a > ( 255/(circle_array[i].base_ticks/2))){
+					circle_array[i].color.a -= ( 255/(circle_array[i].base_ticks/2) );
+				}
+				else{
+					circle_array[i].color.a = 0;
+				}
+			}
 
-                        --circle_array[i].remaining_ticks;
+			--circle_array[i].remaining_ticks;
 
-                        if(circle_array[i].remaining_ticks >=0){
-                                filledCircleRGBA(renderer, circle_array[i].x, circle_array[i].y, circle_array[i].radius,
-                        circle_array[i].color.r, circle_array[i].color.g, circle_array[i].color.b, circle_array[i].color.a);
+			if(circle_array[i].remaining_ticks >=0){
+				filledCircleRGBA(renderer, circle_array[i].x, circle_array[i].y, circle_array[i].radius,
+			circle_array[i].color.r, circle_array[i].color.g, circle_array[i].color.b, circle_array[i].color.a);
 
-                        }
-                        else{
-                                printf("\033[34mCirculo expirado! (-1 ponto)\033[0m\n");
+			}
+			else{
+				printf("\033[34mCirculo expirado! (-1 ponto)\033[0m\n");
                                 /*
                                  *
                                  *      Criar efeito de mensagem de circulo nao clicado aqui!
@@ -214,57 +215,58 @@ int main(int argc, char* argv[]) {
                                 if (score > 0){
                                         --score;
                                 }
-                        }
-                }
-        }
+			}
+		}
+	}
 
-        // Linhas
-        SDL_SetRenderDrawColor(renderer, 0xFF,0xFF,0xFF,0xFF);
+	// Linhas
+	SDL_SetRenderDrawColor(renderer, 0xFF,0xFF,0xFF,0xFF);
 
-        SDL_Rect line1 = {PARTITION_WIDTH, 0, 1, WINDOW_HEIGHT};
-        SDL_Rect line2 = {2*PARTITION_WIDTH, 0, 1, WINDOW_HEIGHT};
+	SDL_Rect line1 = {PARTITION_WIDTH, 0, 1, WINDOW_HEIGHT};
+	SDL_Rect line2 = {2*PARTITION_WIDTH, 0, 1, WINDOW_HEIGHT};
 
-        SDL_RenderFillRect(renderer, &line1);
-        SDL_RenderFillRect(renderer, &line2);
+	SDL_RenderFillRect(renderer, &line1);
+	SDL_RenderFillRect(renderer, &line2);
 
-        // Mostra desenho
-        SDL_RenderPresent(renderer);
+	// Mostra desenho
+	SDL_RenderPresent(renderer);
 
-        //while (SDL_PeepEvents(&event, 1, SDL_GETEVENT, SDL_MOUSEMOTION, SDL_MOUSEMOTION));
-        SDL_EventState(SDL_MOUSEMOTION, SDL_IGNORE);
+	//while (SDL_PeepEvents(&event, 1, SDL_GETEVENT, SDL_MOUSEMOTION, SDL_MOUSEMOTION));
+	SDL_EventState(SDL_MOUSEMOTION, SDL_IGNORE);
 
-                        /*      Checa eventos           */
-        int isevt = AUX_WaitEventTimeoutCount(&event, &tickTime);
+			/*	Checa eventos		*/
+	int isevt = AUX_WaitEventTimeoutCount(&event, &tickTime);
         if (isevt) {
-                switch(event.type){
-                        case SDL_QUIT:
-                                state = 0;
-                                break;
-                        case SDL_MOUSEBUTTONDOWN:
-                                // Verifica se acertou o clique no circulo
-                                int colision_result = check_collision_circle(&score);
-                                switch(colision_result){
-                                        case INT_MIN:
-                                                printf("sei la oque deu\n");
-                                                break;
-                                        case -1:
-                                                printf("Nao houve colisao! (-1 ponto)\n");
-                                                if (score > 0){
-                                                        --score;
-                                                }
-                                                break;
-                                        default:
-                                                printf("Colisao bem-sucedida! (circulo na posicao: %d)\n", colision_result);
-                                                ++score;
-                                }
-                                break;
-
-                }
-        }
-        else{
-                tickTime = 17;
-                ++tick_counter;       // Incrementa ticks
-        }
+		switch(event.type){
+			case SDL_QUIT:
+	                	state = 0;
+				break;
+	    		case SDL_MOUSEBUTTONDOWN:
+		                // Verifica se acertou o clique no circulo
+                		int colision_result = check_collision_circle(&score);
+				switch(colision_result){
+					case INT_MIN:
+						printf("sei la oque deu\n");
+						break;
+					case -1:
+						printf("Nao houve colisao! (-1 ponto)\n");
+						if (score > 0){
+							--score;
+						}
+						break;
+					default:
+						printf("Colisao bem-sucedida! (circulo na posicao: %d)\n", colision_result);
+						++score;
+				}
+				break;
+	
+        	}
+	}
+	else{
+		tickTime = 17;
+		++tick_counter;       // Incrementa ticks
+		printf("incrementa tick_counter! (tick_counter: %d. next_note_tick: %d)\n", tick_counter, next_note_tick);
+	}
 
     }
     printf("\033[36mFim de Jogo! Sua pontuacao e: %d\033[0m\n", score);
@@ -277,18 +279,18 @@ int main(int argc, char* argv[]) {
 
 
 FIM:
-        /*      Desaloca tudo   */
-        /*      Finaliza SDL    */
+    	/*	Desaloca tudo	*/
+	/*	Finaliza SDL	*/
     if(renderer){
-            SDL_DestroyRenderer(renderer);
+	    SDL_DestroyRenderer(renderer);
     }
     if(window){
-            SDL_DestroyWindow(window);
+	    SDL_DestroyWindow(window);
     }
 
      SDL_Quit();
 
-        /*      Retorna funcao */
+    	/*	Retorna funcao */
     // 0-> sucesso
     // <0 -> erro
     return output;
@@ -298,7 +300,8 @@ FIM:
 //Funcao auxiliar que esconde o uso de "SDL_WaitEventTimeout"
 //
 //
-//Atualizada -> Agora nao precisa de getTicks() anterior a chamada
+// Atualizada -> Sempre tentar reduzir tempo, nao somente quando houver evento
+// parece ter consertado problemas de travamento
 int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms){
 
         //Pega tempo anterior
@@ -306,8 +309,10 @@ int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms){
 
         int isevt =  SDL_WaitEventTimeout( evt , *ms );
 
+	Uint32 decorrido = SDL_GetTicks() - antes;
+
         //Lida com atualizacao do valor de espera
-        (*ms) = (isevt)? ((*ms)-( SDL_GetTicks()- antes )):(*ms);
+        *ms = (*ms > decorrido) ? (*ms - decorrido) : 0;
 
         return isevt;
 }
@@ -316,10 +321,10 @@ int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms){
 //
 // Gera circulo aleatorio
 void generate_random_circle(Circle* circle) {
-    circle->partition = rand() % 3;     // Define particao
+    circle->partition = rand() % 3;	// Define particao
 
 
-    circle->starting_tick = 15 + (rand() % 46); // Circulo inicia entre [0,25s;1s]
+    circle->starting_tick = 15 + (rand() % 46);	// Circulo inicia entre [0,25s;1s]
 
     circle-> base_ticks = 180 + (rand() % 301); // Circulos no intervalo de [3s;8s]
 
@@ -337,35 +342,37 @@ void generate_random_circle(Circle* circle) {
     circle->color.a = 0;
 }
 
+// Checa colisao de circulo
+//
+// retorna posicao do buffer se houve colisao
+// se nao, retorna -1
+//
+// INT_MIN -> a
 int check_collision_circle() {
 
     int mouse_x = INT_MIN;
     int mouse_y = INT_MIN;
     SDL_GetMouseState(&mouse_x, &mouse_y);
 
-    if( mouse_x == INT_MIN
-     || mouse_y == INT_MIN      ){
-        printf("Erro! colisao inesperada!");
-        return INT_MIN;
+    if(	mouse_x == INT_MIN
+     ||	mouse_y == INT_MIN	){
+	printf("Erro! Falha ao obter informacoes do mouse!\n");
+	return INT_MIN;
     }
 
     int output = -1;
 
-    if (circle_array_end == circle_array_begin){
-        return output;
-    }
+    for(int i = ((circle_array_end-1 + 100)%100); i != ((circle_array_begin-1 + 100 )%100); i=((i-1+ 100)%100)){
+	int dx = mouse_x - circle_array[i].x;
+	int dy = mouse_y - circle_array[i].y;
 
-    for(int i = circle_array_begin; i != circle_array_end; i=((i+1)%100)){
-        int dx = mouse_x - circle_array[i].x;
-        int dy = mouse_y - circle_array[i].y;
+	if( 	( (dx * dx + dy * dy) <= (circle_array[i].radius * circle_array[i].radius) )
+		&& (circle_array[i].remaining_ticks >= 0) ){
 
-        if(     ( (dx * dx + dy * dy) <= (circle_array[i].radius * circle_array[i].radius) )
-                && (circle_array[i].remaining_ticks >= 0) ){
-
-                        circle_array[i].remaining_ticks = -1;   // Remove circulo
-                        output = i;
-                        break;
-        }
+			circle_array[i].remaining_ticks = -1;	// Remove circulo
+			output = i;
+			break;
+	}
     }
 
     return output;
@@ -373,10 +380,10 @@ int check_collision_circle() {
 
 // Enche array com circulos vazios
 void initiate_circle_array(Circle* array, int array_size){
-        for (int i = 0; i < array_size; i++){
+	for (int i = 0; i < array_size; i++){
 
                 circle_array[i].x = circle_array[i].y = circle_array[i].radius =
-                                circle_array[i].partition = circle_array[i].remaining_ticks = circle_array[i].base_ticks = circle_array[i].starting_tick = -1;
+				circle_array[i].partition = circle_array[i].remaining_ticks = circle_array[i].base_ticks = circle_array[i].starting_tick = -1;
 
                 circle_array[i].color.r = circle_array[i].color.g = circle_array[i].color.b = circle_array[i].color.a = 0x00;
         }
@@ -386,8 +393,8 @@ void initiate_circle_array(Circle* array, int array_size){
 //
 // Enche array com circulos validos aleatorios
 void fill_circle_array(Circle* array, int array_size){
-        for (int i = 0; i <= array_size; i++){
-                generate_random_circle(&array[i]);
+	for (int i = 0; i < array_size; i++){
+		generate_random_circle(&array[i]);
         }
 }
 
@@ -395,9 +402,6 @@ void fill_circle_array(Circle* array, int array_size){
 
 /*
 int music_routine(){
-
-
-
 
 
 }
